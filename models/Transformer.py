2e4448269a7e620432d7dc43bf7a3d5971ee89f6
peng <3034519912@qq.com>
2023-11-29 21:42:25 +0800

    add models

---
+++ models/Transformer.py
@@ -0,0 +1,184 @@
+# -*- coding:utf-8 -*-
+# author:peng
+# Date：2023/11/20 11:29
+import cv2
+import torch
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import numpy as np
+from einops import rearrange, repeat
+
+def flip_horizontal(img, mask):
+    img = np.flip(img, axis=1)
+    mask = np.flip(mask, axis=1)
+    return img, mask
+
+
+def rotate(img, mask, angle_abs=5):
+    h, w, _ = img.shape
+    angle = random.choice([angle_abs, -angle_abs])
+
+    M = cv2.getRotationMatrix2D((h, w), angle, 1.0)
+    img = cv2.warpAffine(img, M, (h, w), flags=cv2.INTER_CUBIC)
+    mask = cv2.warpAffine(mask, M, (h, w), flags=cv2.INTER_CUBIC)
+    mask = np.expand_dims(mask, axis=-1)
+    return img, mask
+
+
+class RandomAugmentation:
+    augmentations = [flip_horizontal, rotate]
+
+    def __init__(self, max_augment_count):
+        if max_augment_count <= len(self.augmentations):
+            self.max_augment_count = max_augment_count
+        else:
+            self.max_augment_count = len(self.augmentations)
+
+    def __call__(self, sample):
+        img, mask = sample['img'], sample['mask']
+
+        augmentation_count = random.randint(0, self.max_augment_count)
+        selected_augmentations = random.sample(self.augmentations, k=augmentation_count)
+        for augmentation in selected_augmentations:
+            img, mask = augmentation(img, mask)
+
+        return {'img': img, 'mask': mask}
+
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, embedding_dim, head_num):
+        super().__init__()
+
+        self.head_num = head_num
+        self.dk = (embedding_dim // head_num) ** (1 / 2)
+
+        self.qkv_layer = nn.Linear(embedding_dim, embedding_dim * 3, bias=False)
+        self.out_attention = nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+    def forward(self, x, mask=None):
+        qkv = self.qkv_layer(x)
+
+        query, key, value = tuple(rearrange(qkv, 'b t (d k h ) -> k b h t d ', k=3, h=self.head_num))
+        energy = torch.einsum("... i d , ... j d -> ... i j", query, key) * self.dk
+
+        if mask is not None:
+            energy = energy.masked_fill(mask, -np.inf)
+
+        attention = torch.softmax(energy, dim=-1)
+
+        x = torch.einsum("... i j , ... j d -> ... i d", attention, value)
+
+        x = rearrange(x, "b h t d -> b t (h d)")
+        x = self.out_attention(x)
+
+        return x
+
+class MLP(nn.Module):
+    def __init__(self, embedding_dim, mlp_dim):
+        super().__init__()
+
+        self.mlp_layers = nn.Sequential(
+            nn.Linear(embedding_dim, mlp_dim),
+            nn.GELU(),
+            nn.Dropout(0.1),
+            nn.Linear(mlp_dim, embedding_dim),
+            nn.Dropout(0.1)
+        )
+
+    def forward(self, x):
+        x = self.mlp_layers(x)
+
+        return x
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self, embedding_dim, head_num, mlp_dim):
+        super().__init__()
+
+        self.multi_head_attention = MultiHeadAttention(embedding_dim, head_num)
+        self.mlp = MLP(embedding_dim, mlp_dim)
+
+        self.layer_norm1 = nn.LayerNorm(embedding_dim)
+        self.layer_norm2 = nn.LayerNorm(embedding_dim)
+
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        _x = self.multi_head_attention(x)
+        _x = self.dropout(_x)
+        x = x + _x
+        x = self.layer_norm1(x)
+
+        _x = self.mlp(x)
+        x = x + _x
+        x = self.layer_norm2(x)
+
+        return x
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, embedding_dim, head_num, mlp_dim, block_num=12):
+        super().__init__()
+
+        self.layer_blocks = nn.ModuleList(
+            [TransformerEncoderBlock(embedding_dim, head_num, mlp_dim) for _ in range(block_num)])
+
+    def forward(self, x):
+        for layer_block in self.layer_blocks:
+            x = layer_block(x)
+
+        return x
+
+class ViT(nn.Module):
+    def __init__(self, img_dim, in_channels, embedding_dim, head_num, mlp_dim,
+                 block_num, patch_dim, classification=True, num_classes=1):
+        super().__init__()
+
+        self.patch_dim = patch_dim
+        self.classification = classification
+        self.num_tokens = (img_dim // patch_dim) ** 2
+        self.token_dim = in_channels * (patch_dim ** 2)
+
+        self.projection = nn.Linear(self.token_dim, embedding_dim)
+        self.embedding = nn.Parameter(torch.rand(self.num_tokens + 1, embedding_dim))
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embedding_dim))
+
+        self.dropout = nn.Dropout(0.1)
+
+        self.transformer = TransformerEncoder(embedding_dim, head_num, mlp_dim, block_num)
+
+        if self.classification:
+            self.mlp_head = nn.Linear(embedding_dim, num_classes)
+
+    def forward(self, x):
+        img_patches = rearrange(x,
+                                'b c (patch_x x) (patch_y y) -> b (x y) (patch_x patch_y c)',
+                                patch_x=self.patch_dim, patch_y=self.patch_dim)
+
+        batch_size, tokens, _ = img_patches.shape
+
+        project = self.projection(img_patches)
+        token = repeat(self.cls_token, 'b ... -> (b batch_size) ...',
+                       batch_size=batch_size)
+
+        patches = torch.cat([token, project], dim=1)
+        patches += self.embedding[:tokens + 1, :]
+
+        x = self.dropout(patches)
+        x = self.transformer(x)
+        x = self.mlp_head(x[:, 0, :]) if self.classification else x[:, 1:, :]
+
+        return x
+
+if __name__ == '__main__':
+    vit = ViT(img_dim=128,
+              in_channels=3,
+              patch_dim=16,
+              embedding_dim=512,
+              block_num=6,
+              head_num=4,
+              mlp_dim=1024)
+    print(sum(p.numel() for p in vit.parameters()))
+    print(vit(torch.rand(1, 3, 128, 128)).shape)
